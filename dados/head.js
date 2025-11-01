b65c5ef5c041cbb19484b56b0ca3d7008867d1da
simulab <erik_b_dos-santos@estudante.sesisenai.org.br>
2025-11-01 01:22:38 -0300

    Update head.js

--- dados/head.js
+++ dados/head.js
@@ -4,7 +4,10 @@
  */
 
 // 1. DADOS DE SUGESTÕES DE PESQUISA (Seu índice de busca)
+
+
 const SEARCH_INDEX = [
+
   // ---------- SIMULADORES: MATEMÁTICA ----------
   { term: "Sim. Cálculo de Matriz", path: "Simulador > Matemática > Simulador", url: "simulacoes/matriz.html" },
   { term: "Sim. Análise de Funções", path: "Simulador > Matemática > Análise", url: "simulacoes/funcoes.html" },
@@ -74,15 +77,12 @@ const SEARCH_INDEX = [
   { term: "Sistema Esquelético", path: "Conteúdo > Biologia > Corpo", url: "biologia/esqueletico.html" },
   { term: "Sistema Urinário", path: "Conteúdo > Biologia > Corpo", url: "biologia/urinario.html" },
   { term: "Sistema Reprodutor", path: "Conteúdo > Biologia > Corpo", url: "biologia/reprodutor.html" },
-
   { term: "Classificação Animal", path: "Conteúdo > Biologia > Animal", url: "biologia/classificacao.html" },
   { term: "Comportamento Animal", path: "Conteúdo > Biologia > Animal", url: "biologia/comportamento.html" },
   { term: "Fisiologia Animal", path: "Conteúdo > Biologia > Animal", url: "biologia/fisiologia.html" },
-
   { term: "DNA e RNA", path: "Conteúdo > Biologia > Genética", url: "biologia/dna.html" },
   { term: "Herança Genética", path: "Conteúdo > Biologia > Genética", url: "biologia/heranca.html" },
   { term: "Mutação", path: "Conteúdo > Biologia > Genética", url: "biologia/mutacao.html" },
-
   { term: "Estrutura Celular", path: "Conteúdo > Biologia > Células", url: "biologia/estrutura-celular.html" },
   { term: "Divisão Celular", path: "Conteúdo > Biologia > Células", url: "biologia/divisao.html" },
   { term: "Metabolismo Celular", path: "Conteúdo > Biologia > Células", url: "biologia/metabolismo.html" },
@@ -92,15 +92,12 @@ const SEARCH_INDEX = [
   { term: "MRUV", path: "Conteúdo > Física > Mecânica", url: "fisica/mruv.html" },
   { term: "Energia", path: "Conteúdo > Física > Mecânica", url: "fisica/energia.html" },
   { term: "Queda Livre", path: "Conteúdo > Física > Mecânica", url: "fisica/queda.html" },
-
   { term: "Dilatação Térmica", path: "Conteúdo > Física > Termodinâmica", url: "fisica/dilatacao.html" },
   { term: "Calorimetria", path: "Conteúdo > Física > Termodinâmica", url: "fisica/calorimetria.html" },
   { term: "Escalas Termométricas", path: "Conteúdo > Física > Termodinâmica", url: "fisica/escalas.html" },
-
   { term: "Ondas e Óptica", path: "Conteúdo > Física > Ondas", url: "fisica/ondas.html" },
   { term: "Hidrostática", path: "Conteúdo > Física > Ondas", url: "fisica/hidrostatica.html" },
   { term: "Gravitação Universal", path: "Conteúdo > Física > Ondas", url: "fisica/gravidade.html" },
-
   { term: "Eletricidade", path: "Conteúdo > Física > Eletromagnetismo", url: "fisica/eletricidade.html" },
   { term: "Eletrostática", path: "Conteúdo > Física > Eletromagnetismo", url: "fisica/eletrostatica.html" },
   { term: "Eletromagnetismo", path: "Conteúdo > Física > Eletromagnetismo", url: "fisica/eletromagnetismo.html" },
@@ -113,13 +110,11 @@ const SEARCH_INDEX = [
   { term: "Ácidos e Bases", path: "Conteúdo > Química > Geral", url: "quimica/acidos-bases.html" },
   { term: "Balanceamento", path: "Conteúdo > Química > Geral", url: "quimica/balanceamento.html" },
   { term: "Funções Inorgânicas", path: "Conteúdo > Química > Geral", url: "quimica/funcoes-inorganicas.html" },
-
   { term: "NOX", path: "Conteúdo > Química > Inorgânica", url: "quimica/nox.html" },
   { term: "Estequiometria", path: "Conteúdo > Química > Inorgânica", url: "quimica/estequiometria.html" },
   { term: "Soluções", path: "Conteúdo > Química > Inorgânica", url: "quimica/solucoes.html" },
   { term: "Reações Químicas", path: "Conteúdo > Química > Inorgânica", url: "quimica/reacoes.html" },
   { term: "Cinética Química", path: "Conteúdo > Química > Inorgânica", url: "quimica/cinetica.html" },
-
   { term: "Orgânica", path: "Conteúdo > Química > Orgânica", url: "quimica/organica.html" },
   { term: "Eletroquímica", path: "Conteúdo > Química > Orgânica", url: "quimica/eletroquimica.html" },
   { term: "Termoquímica", path: "Conteúdo > Química > Orgânica", url: "quimica/termoquimica.html" },
@@ -130,19 +125,26 @@ const SEARCH_INDEX = [
   { term: "Algoritmos", path: "Conteúdo > Programação > Básica", url: "programacao/algoritmos.html" },
   { term: "Variáveis e Tipos", path: "Conteúdo > Programação > Básica", url: "programacao/variaveis.html" },
   { term: "Estruturas de Controle", path: "Conteúdo > Programação > Básica", url: "programacao/controle.html" },
-
   { term: "Funções", path: "Conteúdo > Programação > Avançada", url: "programacao/funcoes.html" },
   { term: "POO", path: "Conteúdo > Programação > Avançada", url: "programacao/poo.html" },
   { term: "Recursão", path: "Conteúdo > Programação > Avançada", url: "programacao/recursao.html" },
-
   { term: "HTML & CSS", path: "Conteúdo > Programação > Web", url: "programacao/html-css.html" },
   { term: "JavaScript", path: "Conteúdo > Programação > Web", url: "programacao/javascript.html" },
   { term: "Frameworks", path: "Conteúdo > Programação > Web", url: "programacao/frameworks.html" },
-
   { term: "SQL", path: "Conteúdo > Programação > Banco de Dados", url: "programacao/sql.html" },
   { term: "NoSQL", path: "Conteúdo > Programação > Banco de Dados", url: "programacao/nosql.html" },
-  { term: "Modelagem de Dados", path: "Conteúdo > Programação > Banco de Dados", url: "programacao/modelagem.html" }
+  { term: "Modelagem de Dados", path: "Conteúdo > Programação > Banco de Dados", url: "programacao/modelagem.html" },
+
+  // ---------- PÁGINAS PRINCIPAIS DAS MATÉRIAS ----------
+  { term: "Sistemas", path: "Matéria > Matemática", url: "materias/sistemas.html" },
+  { term: "Biologia", path: "Matéria > Biologia", url: "materias/biologia.html" },
+  { term: "Física", path: "Matéria > Física", url: "materias/fisica.html" },
+  { term: "Matemática", path: "Matéria > Matemática", url: "materias/matematica.html" },
+  { term: "Programação", path: "Matéria > Programação", url: "materias/programacao.html" },
+  { term: "Química", path: "Matéria > Química", url: "materias/quimica.html" }
+
 ];
+
 
 
 
